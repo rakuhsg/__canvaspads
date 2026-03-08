ab82e66983cc63f36bb82f3bf118e1f3e2b571c1
Hasegawa, Raku <raku@exhibition.jp>
2026-03-08 20:44:47 +0900

    zintl: Remove unused macro

--- ui/zintl/src/lib.rs
+++ ui/zintl/src/lib.rs
@@ -6,18 +6,3 @@ pub mod hook;
 pub mod sequence;
 pub mod store;
 pub mod view;
-
-#[macro_export]
-macro_rules! elm {
-    {$($e:expr),+} => {
-        {
-            $crate::element::Element::Packed {
-                inner: vec![
-                    $(
-                        Box::new($e),
-                    )+
-                ],
-            }
-        }
-    };
-}
